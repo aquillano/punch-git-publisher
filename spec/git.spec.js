2ac9be78758ef8e3fcda44d33fccde630a26155c
Steve Aquillano <steveaquillano@gmail.com>
2012-12-18 16:52:02 -0800

    Stub out files

---
+++ spec/git.spec.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+	git_publisher = require('../lib/git.js');
+
+describe('calling publish', function() {
+
+	it('show retrieve git options from the config', function() {
+
+	});
+
+	it('return a gitty instance', function() {
+
+	});
+
+});
